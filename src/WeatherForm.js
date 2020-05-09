0b191d0b3b73cc70188bf1696a27d97ba0a6731f
Isobel-nixon <nixonisobel@gmail.com>
2020-05-09 19:05:42 +1000

    Add gif API call

--- src/WeatherForm.js
+++ src/WeatherForm.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import {WeatherResult} from './WeatherResult';
 import {PostcodeChange} from './PostcodeChange';
-import GetWeather from './GetWeather'
+import GetWeather from './GetWeather';
+import WeatherGif from './WeatherGif';
+import Tenor from './Tenor';
 
 export class WeatherForm extends React.Component{
     constructor(props){
         super(props);
-        this.state = {description: '', name: ''};
+        this.state = {description: '', name: '', gifSrc: ''};
         this.getWeather = this.getWeather.bind(this);
+        this.getGif = this.getGif.bind(this);
+    }
+
+    getGif(description){
+        Tenor.getGif(description).then(gifSrc =>{
+            this.setState({gifSrc: gifSrc})
+        })
     }
     
     getWeather(postcode){
         GetWeather.getWeather(postcode).then(weather=>{
             this.setState({description: weather.description, name: weather.name});
-        })
+            this.getGif(this.state.description);
+        });
     }
 
     render(){
         return (
             <div>
+                <WeatherGif src={this.state.gifSrc}/>
                 <WeatherResult description={this.state.description} name={this.state.name}/>
                 <PostcodeChange onSubmit = {this.getWeather}/>
             </div>
