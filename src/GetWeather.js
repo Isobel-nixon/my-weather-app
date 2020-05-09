991ba97c942bec45a2f67777a9e89dd874630f80
Isobel-nixon <nixonisobel@gmail.com>
2020-05-09 18:01:15 +1000

    Change API call to be over HTTPS

--- src/GetWeather.js
+++ src/GetWeather.js
@@ -1,7 +1,7 @@
 
 const getWeather = (postcode) => {
     const openWeatherKey = 'bdd454f47dc7feddacfe5125750ba300';
-    const url = `http://api.openweathermap.org/data/2.5/weather?zip=${postcode},AU&appid=${openWeatherKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${postcode},AU&appid=${openWeatherKey}`;
     return fetch(url)
     .then(res => res.json())
     .then(jsonResponse => {
